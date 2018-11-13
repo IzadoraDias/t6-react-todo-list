a556750859aa3f51ccae6fe8ddc710d993439f7b
IzadoraDias <izzadoradias@gmail.com>
2018-11-13 16:43:08 -0200

    dissabled button

--- my-app/src/components/form/input/input.js
+++ my-app/src/components/form/input/input.js
@@ -15,44 +15,50 @@ import './input.css'
 class Input extends React.Component {
     constructor(props) {
         super(props)
-        this.state= { message : false }
+        this.state = { message: null }
     }
+
+    hasError = () => {
+        if (this.state.message) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+
     handleChange = (e) => {
         const value = e.target.value
         let message = ''
         const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        
+
         console.log(value)
-        if(this.props.required && value.trim() === ''){
+        if (this.props.required && value.trim() === '') {
             message = 'Campo Obrigatótio'
-        }else if (this.props.minLength && value.length < this.props.minLength){
-            message =  `Digite pelo menos ${this.props.minLength} caracteres`
-        }else if (this.props.type==='email' && !regex.test(value)){
+        } else if (this.props.minLength && value.length < this.props.minLength) {
+            message = `Digite pelo menos ${this.props.minLength} caracteres`
+        } else if (this.props.type === 'email' && !regex.test(value)) {
             message = `Digite um e-mail válido`
         }
-        this.setState({ message : message })
+        this.setState({ message: message }, this.props.onChange())
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
-            <input
-            onChange= {this.handleChange}
-            onBlur={this.handleChange}
-            type={this.props.type}
-            id={this.props.id}
-            className='input'
-            placeholder={this.props.placeholder}
-            required={this.props.required}
-        />
-          <p className= 'input__erro'>{this.state.message}</p>
-           </React.Fragment>
+                <input
+                    onChange={this.handleChange}
+                    onBlur={this.handleChange}
+                    type={this.props.type}
+                    id={this.props.id}
+                    className='input'
+                    placeholder={this.props.placeholder}
+                    required={this.props.required}
+                />
+                <p className='input__erro'>{this.state.message}</p>
+            </React.Fragment>
         )
     }
-
-
-
-
 
 }
 
--- my-app/src/pages/login/login.js
+++ my-app/src/pages/login/login.js
@@ -3,18 +3,46 @@ import Form from '../../components/form'
 import Container from '../../components/container'
 
 
-function Login() {
-    return (
-        <Container>
+// function Login() {
+//     return (
+//         <Container>
+//             <Form title='Login' text='Entre com Seu email e Senha'>
+//                 <Form.Label htmlFor='Email'>Email</Form.Label>
+//                 <Form.Input id='Email' type='email' required/>
+//                 <Form.Label htmlFor='password'>Senha</Form.Label>
+//                 <Form.Input id='password' type='password' minLength={6} required/>
+//                 <Form.Button disabled={this.state.disabled}>Enviar</Form.Button>
+//                 <Form.Link href='#'>Criar uma conta</Form.Link>
+//             </Form>
+//         </Container>
+//     )
+// }
+// export default Login 
+
+class Login extends React.Component{
+    constructor(){
+        super()
+        this.state= { disabled : true }
+}
+
+onDisabledButton = () => {
+    console.log('oi')
+}
+
+    render(){
+        return (
+            <Container>
             <Form title='Login' text='Entre com Seu email e Senha'>
                 <Form.Label htmlFor='Email'>Email</Form.Label>
-                <Form.Input id='Email' type='email' required/>
+                <Form.Input id='Email' type='email' onChange={this.onDisabledButton} required/>
                 <Form.Label htmlFor='password'>Senha</Form.Label>
-                <Form.Input id='password' type='password' minLength={6} required/>
-                <Form.Button disabled>Enviar</Form.Button>
+                <Form.Input id='password' type='password' onChange={this.onDisabledButton} minLength={6} required/>
+                <Form.Button disabled={this.state.disabled}>Enviar</Form.Button>
                 <Form.Link href='#'>Criar uma conta</Form.Link>
             </Form>
         </Container>
-    )
+        )
+    }
 }
-export default Login 
+
+export default Login
