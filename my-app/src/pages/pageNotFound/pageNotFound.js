c2b77eaae86de6699b4b0ecc84dd80d4f454c92e
IzadoraDias <izadoradias@gmail.com>
2018-11-14 14:21:08 -0200

    criei a pageNotFound e a rota

---
+++ my-app/src/pages/pageNotFound/pageNotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './pageNotFound.js'
+
+
+function PageNotFound() {
+
+    return (
+        <div class="pageNotFound">
+            <h1>Page Not Found </h1>
+        </div>
+
+    )
+}
+
+
+export default PageNotFound 
