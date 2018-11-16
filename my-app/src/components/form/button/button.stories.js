a1a66cebb0f97a2673750d81646fc1ec90fa9e43
Izzadoradias@gmail.com <Izzadoradias@gmail.com>
2018-11-16 09:52:34 -0200

    adicionei disabled button

--- my-app/src/components/form/button/button.stories.js
+++ my-app/src/components/form/button/button.stories.js
@@ -4,4 +4,9 @@ import Button from './index'
 
 
 storiesOf('Components/Form/Button', module)
- .add('default', () => (<Button> Children Here </Button>))
+ .add('default', () => (
+ <Button> Children Here </Button>
+ ))
+ .add('with disabled', () =>(
+     <Button disabled> Children Here </Button>
+ ))
