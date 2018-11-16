050ae634925a3846cb469c58a2127c612ffb8b1c
Izzadoradias@gmail.com <Izzadoradias@gmail.com>
2018-11-16 10:12:24 -0200

    criei container.stories

---
+++ my-app/src/components/container/container.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Container  from './index'
+
+storiesOf ('Components/container', module)
+.add('default', () => (
+    <Container>
+        Children Here
+    </Container>
+
+))
+
+
+
